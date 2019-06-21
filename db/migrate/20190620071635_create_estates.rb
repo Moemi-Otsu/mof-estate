a48b92560a563fe9c506af84c19fabe6b81e8432
moemi ohtsu <s0623192@gmail.com>
2019-06-21 14:18:50 +0900

    Feat: estateモデルをマイグレーション実行

--- db/migrate/20190620071635_create_estates.rb
+++ db/migrate/20190620071635_create_estates.rb
@@ -1,6 +1,11 @@
 class CreateEstates < ActiveRecord::Migration[5.0]
   def change
     create_table :estates do |t|
+      t.text "property_name"
+      t.integer "rent"
+      t.text "address"
+      t.integer "property_age"
+      t.text "content"
 
       t.timestamps
     end
