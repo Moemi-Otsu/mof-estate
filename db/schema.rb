22f03956a1e162f63474a8aa9ebc0e76d886de33
moemi ohtsu <s0623192@gmail.com>
2019-06-21 15:44:58 +0900

    Feat: stationモデルにカラムを追加してmigrationを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190620071635) do
+ActiveRecord::Schema.define(version: 20190621064108) do
 
   create_table "estates", force: :cascade do |t|
     t.text     "property_name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20190620071635) do
     t.text     "content"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "route_name"
+    t.string   "station_name"
+    t.integer  "walking_minutes"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
