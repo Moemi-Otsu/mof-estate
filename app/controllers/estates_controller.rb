d129391858bbafcff5c39a1c5b7ee7efe2cb3aae
moemi ohtsu <s0623192@gmail.com>
2019-06-22 12:08:47 +0900

    Feat: 新規投稿機能作成（まだネストの情報は取れていない状態）

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -5,11 +5,21 @@ class EstatesController < ApplicationController
     @stations = Station.all
   end
 
+  def new
+    @estate = Estate.new
+  end
+
+  def create
+    Estate.create(estate_params)
+    redirect_to new_estate_path
+  end
+
   def show
     @estate = Estate.find(params[:id])
   end
 
   def edit
+    @estate = Estate.find(params[:id])
   end
 
   def destroy
