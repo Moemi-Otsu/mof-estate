d6e32248c5f3dba7af918e5c2f532adbf6f0f20d
moemi ohtsu <s0623192@gmail.com>
2019-06-23 22:16:07 +0900

    Feat: 日本語化（一部バグあり)、整数値入力のバリデーション設定

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -2,7 +2,7 @@ class Estate < ApplicationRecord
   validates :property_name, presence: true
   validates :rent, presence: true
   validates :address, presence: true
-  validates :property_age, presence: true
+  validates :property_age, presence: true, numericality: { only_integer: true, greater_than: 1 }
   validates :content, presence: true
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,6 @@
 class Station < ApplicationRecord
   validates :route_name, presence: true
   validates :station_name, presence: true
-  validates :walking_minutes, presence: true
+  validates :walking_minutes, presence: true, numericality: { only_integer: true, greater_than: 1 }
   belongs_to :estate, optional: true
 end
