0b0cc58cb2231a3b61751f61b5b2295d702e47d3
moemi ohtsu <s0623192@gmail.com>
2019-06-23 22:17:30 +0900

    Feat: 家賃に整数値のバリデーション設定

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -1,6 +1,6 @@
 class Estate < ApplicationRecord
   validates :property_name, presence: true
-  validates :rent, presence: true
+  validates :rent, presence: true, numericality: { only_integer: true, greater_than: 1 }
   validates :address, presence: true
   validates :property_age, presence: true, numericality: { only_integer: true, greater_than: 1 }
   validates :content, presence: true
