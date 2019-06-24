0679490721fd3b38074f6b2bccc37410455f7c74
moemi ohtsu <s0623192@gmail.com>
2019-06-24 15:51:08 +0900

    Fix: 備考欄を未入力でも通るよう修正・最寄駅が1分でも通るよう修正

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -3,7 +3,6 @@ class Estate < ApplicationRecord
   validates :rent, presence: true, numericality: { only_integer: true, greater_than: 1 }
   validates :address, presence: true
   validates :property_age, presence: true, numericality: { only_integer: true, greater_than: 1 }
-  validates :content, presence: true
   has_many :stations, inverse_of: :estate, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -2,6 +2,6 @@ class Station < ApplicationRecord
   FORM = 2
   # validates :route_name, presence: true
   # validates :station_name, presence: true
-  validates :walking_minutes, numericality: { only_integer: true, greater_than: 1 }
+  validates :walking_minutes, numericality: { only_integer: true, greater_than: 0 }
   belongs_to :estate, inverse_of: :stations, optional: true, foreign_key: 'estate_id'
 end
